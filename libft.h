cf74e2cb76cf35db45b5b69cdb9462a39c3115ac
mkhellou <yoyomcawom@gmail.com>
2022-09-30 10:54:37 +0100

    0.11 add ft_strchr

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mkhellou <mkhellou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 14:41:15 by mkhellou          #+#    #+#             */
-/*   Updated: 2022/09/29 18:40:07 by mkhellou         ###   ########.fr       */
+/*   Updated: 2022/09/30 10:47:19 by mkhellou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,10 @@ int		ft_tolower(int c);
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+char	*ft_strchr(const char *s, int c);
+
+
 
 
 #endif
