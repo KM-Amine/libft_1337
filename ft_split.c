be53bf55f69cd602a37dfd7bf7ce9bdcb7d93af0
mkhellou <yoyomcawom@gmail.com>
2022-10-03 19:13:54 +0100

    i don't know what to say

--- ft_split.c
+++ ft_split.c
@@ -6,36 +6,80 @@
 /*   By: mkhellou <mkhellou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 11:16:32 by mkhellou          #+#    #+#             */
-/*   Updated: 2022/10/03 12:32:13 by mkhellou         ###   ########.fr       */
+/*   Updated: 2022/10/03 19:13:25 by mkhellou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	count(char *trimed, char c)
+static char	*strtrim(char const *s1, char const *set)
 {
-	int		i;
-	char	*inter;
+	int	i;
+	int	j;
 
-	inter = trimed;
+	if (s1 == NULL || set == NULL)
+		return (0);
 	i = 0;
-	while (ft_strchr(inter, c))
-	{
-		inter = ft_strchr(inter, c) + 1;
+	j = ft_strlen(s1) - 1;
+	while (s1[i] && ft_strchr(set, s1[i]))
 		i++;
+	while (s1[j] && ft_strchr(set, s1[j]))
+		j--;
+	return (ft_substr(s1, i, (j - i + 1)));
+}
+
+static int	count_strings(char *trimed, char c)
+{
+	int	i;
+	int	j;
+	int	b;
+	int	len;
+
+	b = 0;
+	i = 0;
+	j = 0;
+	len = ft_strlen(trimed);
+	while (i <= len)
+	{
+		if (trimed[i] == c && b == 0)
+		{
+			j++;
+			b = 1;
+		}
+		if (trimed[i] != c)
+		{
+			b = 0;
+		}
+			i++;
+		printf("%d",j + 1);
 	}
-	i++;
-	return (i);
+	return (j +1);
 }
 
 char	**ft_split(char const *s, char c)
 {
 	char	*trimed;
+	int		count;
+	char	**big;
 	int		i;
-	char	*inter;
+	int		j;
 
-	trimed = ft_strtrim(s, &c);
-	printf("%d", count(trimed, c));
-	free (trimed);
-	return (0);
+	if (!s)
+		return (0);
+	trimed = strtrim(s, &c);
+	count = count_strings(trimed, c);
+	big = (char **)ft_calloc(count + 1, sizeof(char *));
+	j = 0;
+	i = 0;
+	while (j < count)
+	{
+		trimed = trimed + i;
+		while (*trimed == c)
+			trimed = ft_strchr(trimed, c) + 1;
+		i = ft_strchr(trimed, c) - trimed;
+		big[j] = ft_substr(trimed, 0, i);
+		//printf("%s\n", big[j]);
+		j++;
+	}
+	return (big);
 }
