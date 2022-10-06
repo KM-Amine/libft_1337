a9843b28d9e50ac628159ea94dbcf7b3b3d88903
mkhellou <yoyomcawom@gmail.com>
2022-10-06 09:23:14 +0100

    adding ft_split with bad norm

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: mkhellou <mkhellou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 11:16:32 by mkhellou          #+#    #+#             */
-/*   Updated: 2022/10/04 16:51:05 by mkhellou         ###   ########.fr       */
+/*   Updated: 2022/10/06 08:14:08 by mkhellou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,42 +47,6 @@ static char	**free_all(char **big, int j)
 	return (0);
 }
 
-
-// static int	count_strings(char *trimed, char c)
-// {
-// 	int	i;
-
-// 	while (*trimed == c)
-// 		trimed ++;
-// 	while ()
-// 	{
-// 		if (*trimed == c)
-// 			trimed ++;
-// 		if(*trimed != c)
-// 			trimed ++;
-// 			i++;
-// 	}
-// 	i = 0;
-// 	return (i + 1);
-// }
-
-// static int	c_count(char const *trimed, char c)
-// {
-// 	int	i;
-// 	int	len;
-
-// 	i = 0;
-// 	len = ft_strlen(trimed);
-// 	while (ft_strchr(trimed, c))
-// 	{
-// 		trimed = ft_strchr(trimed, c)+1;
-// 		if (*trimed == c)
-// 			continue ;
-// 		i++; 
-// 	}	
-// 	return (i+1);
-// }
-
 char	**ft_split(char const *s, char c)
 {
 	char	*trimed;
@@ -93,11 +57,11 @@ char	**ft_split(char const *s, char c)
 
 	if (!s)
 		return (0);
-	trimed = ft_strtrim(s, &c);
-	if (!trimed)
-		return (0);
+	trimed = (char *)s;
 	count = c_count(trimed, c);
 	big = (char **)ft_calloc(count + 1, sizeof(char *));
+	if (!big)
+		return (0);
 	j = 0;
 	i = 0;
 	while (j < count)
@@ -111,78 +75,5 @@ char	**ft_split(char const *s, char c)
 			return (free_all(big, j));
 		j++;
 	}
-	free(trimed);
 	return (big);
 }
-
-// char **ft_split(char const *s, char c)
-// {
-// 	char*	trimed;
-
-// 	trimed = (char *)s;
-// 	while (*trimed == c)
-// 	{
-// 		trimed++;
-// 	}
-// 	while (*trimed != c)
-// 	{
-// 		trimed ++;
-// 	}
-// 	printf("%s", trimed);
-// 	return (0);
-// }
-
-
-
-
-// static char	*get_word(char const *s, char c, int *index)
-// {
-// 	int		i;
-// 	int		wdlen;
-// 	char	*str;
-
-// 	wdlen = 0;
-// 	i = 0;
-// 	while (s[(*index)] == c)
-// 		(*index)++;
-// 	i = *index;
-// 	while (s[i] && s[i] != c)
-// 	{
-// 		wdlen++;
-// 		i++;
-// 	}
-// 	str = (char *)malloc((wdlen + 1) * (sizeof(char)));
-// 	if (!str)
-// 		return (0);
-// 	i = 0;
-// 	while (s[(*index)] && s[(*index)] != c)
-// 		str[i++] = s[(*index)++];
-// 	str[i] = 0;
-// 	return (str);
-// }
-
-// char	**ft_split(char const *s, char c)
-// {
-// 	int		c_occurence;
-// 	int		i;
-// 	char	**arr;
-// 	int		index;
-
-// 	if (s == NULL)
-// 		return (0);
-// 	index = 0;
-// 	i = 0;
-// 	c_occurence = c_count(s, c);
-// 	arr = (char **)malloc(sizeof(char *) * (c_occurence + 1));
-// 	if (!arr)
-// 		return (0);
-// 	while (i < c_occurence)
-// 	{
-// 		arr[i] = get_word(s, c, &index);
-// 		if (!arr)
-// 			return (free_all(arr, i));
-// 		i++;
-// 	}
-// 	arr[i] = 0;
-// 	return (arr);
-// }
